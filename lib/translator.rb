28652a7ae0529fd50d4937ea066978737a2bcb18
Helen Marshall <marshallhelenm@gmail.com>
2019-07-05 21:18:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,6 @@ def get_japanese_emoticon(file_path, emoticon)
   return j_emoticon ? j_emoticon : "Sorry, that emoticon was not found"
   end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
 end
